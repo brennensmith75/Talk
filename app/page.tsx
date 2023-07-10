2088a1149ae3ec4c0d78472c37c0f2ecee82936a
brennensmith75 <brennens642@gmail.com>
2023-07-10 15:06:02 -0700

    minor type fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import { nanoid } from '@/lib/utils'
 import { Chat } from '@/components/chat'
+import { auth } from '@/auth'
 
 export const runtime = 'edge'
 
-export default function IndexPage() {
+export default async function IndexPage() {
+
+  const session = await auth()
+  const userId = session?.user?.id
   const id = nanoid()
 
-  return <Chat id={id} />
+  return <Chat userId={userId} id={id} />
 }
