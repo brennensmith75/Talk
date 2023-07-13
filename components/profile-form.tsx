931faf6566f47da93efabbae14be60fbc19183c2
brennensmith75 <brennens642@gmail.com>
2023-07-13 10:35:16 -0700

    Refactor(profile-form) update dynamic form schema creation

--- components/profile-form.tsx
+++ components/profile-form.tsx
@@ -1,10 +1,12 @@
 'use client'
 
+// Supabase Auth
 import { type Session } from '@supabase/auth-helpers-nextjs'
+
 
 // Zod
 import { zodResolver } from '@hookform/resolvers/zod'
-import { z } from 'zod'
+import { ZodTypeAny, z } from 'zod'
 
 // UI
 import { Button } from '@/components/ui/button'
@@ -18,28 +20,54 @@ import {
   FormMessage
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
-
 import { useForm } from 'react-hook-form'
-import { Textarea } from './ui/textarea';
+import { Textarea } from './ui/textarea'
 
-const formSchema = z.object({
-  username: z
-    .string()
-    .min(2, {
-      message: 'Username must be at least 2 characters long'
+/**
+ * Create form schema with a dynamic number of system profile prompts based on user's number of profiles.
+
+* @param numProfiles Number of profiles the user has. Initialized to 1 inside ProfileForm component.
+ * @returns Zod object schema
+ */
+const createFormSchema = (numProfiles: number) => {
+  let dynamicSchema: Record<string, ZodTypeAny> = {
+    username: z
+      .string()
+      .min(2, {
+        message: 'Username must be at least 2 characters long'
+      })
+      .max(50),
+    email: z.string().email({
+      message: 'Please enter a valid email address'
     })
-    .max(50),
-  email: z.string().email(),
-  systemPrompt: z.string().max(1000, {
-    message: 'System prompt must be less than 1000 characters long'
-  })
-})
+  }
+
+  for (let i = 0; i < numProfiles; i++) {
+    dynamicSchema[`systemPrompt-profile${i.toString()}`] = z
+      .string()
+      .max(1000, {
+        message: 'System prompt must be less than 1000 characters long'
+      })
+  }
+
+  return z.object(dynamicSchema)
+}
 
 interface ProfileFormProps {
   user: Session['user'] | undefined
 }
 
+/**
+ * Create a form for the user to edit their profile and system prompts.
+ * @param user User object from Supabase
+ * @returns Form component
+ */
 export function ProfileForm({ user }: ProfileFormProps) {
+  // Default to 1 profile if user has no profiles
+  const profileCount = user?.user_metadata.num_profiles ?? 1
+  const formSchema = createFormSchema(profileCount)
+
+  // Populate form with user's existing data from Supabase
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -50,10 +78,10 @@ export function ProfileForm({ user }: ProfileFormProps) {
   })
 
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+    // TODO: Update user metadata in Supabase
     console.log(values)
   }
+  console.log('user', user);
 
   return (
     <Form {...form}>
@@ -90,23 +118,35 @@ export function ProfileForm({ user }: ProfileFormProps) {
             </FormItem>
           )}
         />
+
         <FormField
+          key={`systemPrompt-profile${profileCount.toString()}`}
           control={form.control}
-          name="systemPrompt"
+          name={`systemPrompt-profile${profileCount.toString()}`}
           render={({ field }) => (
             <FormItem>
               <FormLabel>System Prompt</FormLabel>
               <FormControl>
-                <Textarea placeholder="System Prompt here" {...field} />
+                <Textarea
+                  placeholder="I love to learn about technology trends and enjoy humor in responses."
+                  {...field}
+                />
               </FormControl>
               <FormDescription>
-                Add some details about yourself for the system to consider when generating your responses. Provide any information you think will help the AI generate more useful responses.
+                Tailor your AI experience here. Each time you interact with the
+                AI, it refers to this text to personalize its responses. Please
+                keep your description focused and direct. For instance: &quot;I
+                have a deep interest in history and prefer analogies to explain
+                complex concepts.&quot;
               </FormDescription>
               <FormMessage />
             </FormItem>
           )}
         />
-        <Button type="submit" className='dark:bg-secondary dark:text-white'>Submit</Button>
+
+        <Button type="submit" className="dark:bg-secondary dark:text-white">
+          Submit
+        </Button>
       </form>
     </Form>
   )
