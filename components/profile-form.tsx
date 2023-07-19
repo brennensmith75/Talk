2adffcc6541441dbb278aacc00c8ac794c4ce522
brennensmith75 <brennens642@gmail.com>
2023-07-19 09:26:20 -0700

    🚀feat(profile-form) convert async function to use useEffect\n🛠️refactor(profile-form) decouple form schema creation, default values and loading state\n⚙️chore(profile-form) remove unused comments and logs

--- components/profile-form.tsx
+++ components/profile-form.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 // Supabase
-// import { type Session } from '@supabase/auth-helpers-nextjs'
+import { createClientComponentClient, type Session } from '@supabase/auth-helpers-nextjs'
 
 import { updateUser } from '@/app/actions'
 
@@ -23,6 +23,7 @@ import {
 import { Input } from '@/components/ui/input'
 import { useForm } from 'react-hook-form'
 import { Textarea } from './ui/textarea'
+import { useEffect, useState } from 'react';
 
 /**
  * Create form schema.
@@ -73,95 +74,126 @@ const createDefaultFormValues = ({
 /**
  * Create a form for the user to edit their profile and system prompts.
  */
-export async function ProfileForm({
-  user,
-  prompts
+export function ProfileForm({
+  user
 }: {
   user: Session['user']
-  prompts: Record<string, string>
 }) {
-  // Initialize supabase client
+  console.log('🔴 render ProfileForm')
+  console.log('🔴 another one')
+  const [prompts, setPrompts] = useState<Record<string, string>>({})
+  console.log('🔴 prompts state')
+  const [formSchema, setFormSchema] = useState<z.ZodObject<any, any, any>>(z.object({}))
+  const [defaultValues, setDefaultValues] = useState({})
+  const [loading, setLoading] = useState(true)
+  console.log('🔴 after useState')
+  const form = useForm<z.infer<typeof formSchema>>()
 
-  const formSchema = createFormSchema(prompts)
-  const defaultValues = createDefaultFormValues({ user, prompts })
+  useEffect(() => {
+    console.log('🔴 enter useeffect')
+    const getPrompts = async () => {
+      const supabase = createClientComponentClient()
+      const { data, error } = await supabase
+        .from('profiles')
+        .select('prompts')
+        .eq('user_id', user.id)
+        .single()
+      console.log('🔴 after query')
+      if (error) {
+        console.log(error)
+      } else {
+        const retrievedPrompts = data?.prompts
+        setPrompts(retrievedPrompts)
+        setFormSchema(createFormSchema(retrievedPrompts))
+        setDefaultValues(createDefaultFormValues({ user, prompts: retrievedPrompts }))
 
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: defaultValues
-  })
+        // Reset form with the new formSchema and defaultValues
+        form.reset(createDefaultFormValues({ user, prompts: retrievedPrompts }), {
+          resolver: zodResolver(createFormSchema(retrievedPrompts)),
+          errors: {}, // Reset form errors
+        })
+
+        setLoading(false)
+      }
+    }
+    console.log('🔴 call useeffect')
+    getPrompts()
+  }, [form])
+
+  // if (loading) {
+  //   return <div>Loading...</div>
+  // }
 
   async function onSubmit(values: z.infer<typeof formSchema>) {
     let updatedProfileData = { ...values }
     await updateUser({ updatedProfileData, user })
-
   }
 
-  return "hi"
-  // return (
-  //   <Form {...form}>
-  //     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-  //       <FormField
-  //         control={form.control}
-  //         name="username"
-  //         render={({ field }) => (
-  //           <FormItem>
-  //             <FormLabel>Username</FormLabel>
-  //             <FormControl>
-  //               <Input placeholder="smol-developer" {...field} />
-  //             </FormControl>
-  //             <FormMessage />
-  //           </FormItem>
-  //         )}
-  //       />
-  //       <FormField
-  //         control={form.control}
-  //         name="email"
-  //         render={({ field }) => (
-  //           <FormItem>
-  //             <FormLabel>Email</FormLabel>
-  //             <FormControl>
-  //               <Input placeholder="sama@openai.com" {...field} />
-  //             </FormControl>
-  //             <FormMessage />
-  //           </FormItem>
-  //         )}
-  //       />
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="smol-developer" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="sama@openai.com" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
-  //       {/* Dynamically generate form fields for prompts */}
-  //       {Object.keys(prompts).map(prompt => {
+        {/* Dynamically generate form fields for prompts */}
+        {Object.keys(prompts).map(prompt => {
 
-  //         return (
-  //           <FormField
-  //             key={prompt}
-  //             control={form.control}
-  //             name={prompt}
-  //             render={({ field }) => (
-  //               <FormItem>
-  //                 <FormLabel>{prompt}</FormLabel>
-  //                 <FormControl>
-  //                   <Textarea
-  //                     placeholder="I love to learn about technology trends and enjoy humor in responses."
-  //                     {...field}
-  //                   />
-  //                 </FormControl>
-  //                 <FormDescription>
-  //                   Tailor your AI experience here. Each time you interact with
-  //                   the AI, it refers to this text to personalize its responses.
-  //                   Please keep your description focused and direct. For
-  //                   instance: &quot;I have a deep interest in history and prefer
-  //                   analogies to explain complex concepts.&quot;
-  //                 </FormDescription>
-  //                 <FormMessage />
-  //               </FormItem>
-  //             )}
-  //           />
-  //         )
-  //       })}
+          return (
+            <FormField
+              key={prompt}
+              control={form.control}
+              name={prompt}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>{prompt}</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="I love to learn about technology trends and enjoy humor in responses."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Tailor your AI experience here. Each time you interact with
+                    the AI, it refers to this text to personalize its responses.
+                    Please keep your description focused and direct. For
+                    instance: &quot;I have a deep interest in history and prefer
+                    analogies to explain complex concepts.&quot;
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          )
+        })}
 
-  //       <Button type="submit" className="dark:bg-secondary dark:text-white">
-  //         Submit
-  //       </Button>
-  //     </form>
-  //   </Form>
-  // )
+        <Button type="submit" className="dark:bg-secondary dark:text-white">
+          Submit
+        </Button>
+      </form>
+    </Form>
+  )
 }
