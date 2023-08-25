39d73ede8a962fff3444814534ccf9b23b4487f8
brennensmith75 <brennens642@gmail.com>
2023-08-25 14:05:48 -0700

    use vercel json

--- next.config.js
+++ next.config.js
@@ -4,46 +4,6 @@ module.exports = {
   experimental: {
     serverActions: true
   },
-  async redirects() {
-    // https://vercel.com/docs/edge-network/redirects
-    return [
-      {
-        source: '/developer',
-        permanent: false,
-        destination: 'https://github.com/smol-ai/developer'
-      },
-      {
-        source: '/dev',
-        permanent: false,
-        destination: 'https://github.com/smol-ai/developer'
-      },
-      {
-        source: '/godmode',
-        permanent: false,
-        destination: 'https://github.com/smol-ai/GodMode'
-      },
-      {
-        source: '/gods',
-        permanent: false,
-        destination: 'https://github.com/smol-ai/GodMode'
-      },
-      {
-        source: '/god',
-        permanent: false,
-        destination: 'https://github.com/smol-ai/GodMode'
-      },
-      {
-        source: '/haus',
-        permanent: false,
-        destination: 'https://smolai.netlify.app/haus'
-      },
-      {
-        source: '/house',
-        permanent: false,
-        destination: 'https://smolai.netlify.app/haus'
-      }
-    ]
-  },
   images: {
     remotePatterns: [
       {
