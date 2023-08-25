81f869116757d7323bd3b5b987ad58c1397e8efa
brennensmith75 <brennens642@gmail.com>
2023-08-25 13:30:49 -0700

    add redirects

--- next.config.js
+++ next.config.js
@@ -4,6 +4,31 @@ module.exports = {
   experimental: {
     serverActions: true
   },
+  async redirects() {
+    // https://vercel.com/docs/edge-network/redirects
+    return [
+      {
+        source: '/godmode',
+        destination: 'https://github.com/smol-ai/GodMode'
+      },
+      {
+        source: '/gods',
+        destination: 'https://github.com/smol-ai/GodMode'
+      },
+      {
+        source: '/god',
+        destination: 'https://github.com/smol-ai/GodMode'
+      },
+      {
+        source: '/haus',
+        destination: 'https://smolai.netlify.app/haus'
+      },
+      {
+        source: '/house',
+        destination: 'https://smolai.netlify.app/haus'
+      }
+    ]
+  },
   images: {
     remotePatterns: [
       {
