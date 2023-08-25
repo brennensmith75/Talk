b19061636cac642d7b13fbdd64b7371d488aa5c2
brennensmith75 <brennens642@gmail.com>
2023-08-25 13:57:05 -0700

    permanent false

--- next.config.js
+++ next.config.js
@@ -9,22 +9,27 @@ module.exports = {
     return [
       {
         source: '/godmode',
+        permanent: false,
         destination: 'https://github.com/smol-ai/GodMode'
       },
       {
         source: '/gods',
+        permanent: false,
         destination: 'https://github.com/smol-ai/GodMode'
       },
       {
         source: '/god',
+        permanent: false,
         destination: 'https://github.com/smol-ai/GodMode'
       },
       {
         source: '/haus',
+        permanent: false,
         destination: 'https://smolai.netlify.app/haus'
       },
       {
         source: '/house',
+        permanent: false,
         destination: 'https://smolai.netlify.app/haus'
       }
     ]
